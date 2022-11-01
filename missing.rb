0f9130b889ac0d4a1d1c2405c83e437781d66494
Aleh13 <q4wk13@gmail.com>
2022-11-01 21:16:27 +0300

    Русские буквы для Windows

--- missing.rb
+++ missing.rb
@@ -1,1 +1,12 @@
 require 'yaml'
+
+# XXX/ Этот код необходим только при использовании русских букв на Windows
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+# /XXX
